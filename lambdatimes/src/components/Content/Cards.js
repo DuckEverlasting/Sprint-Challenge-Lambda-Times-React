ce9d91a2e00513421f9b5085d7bc863b2debb49e
Matt Klein <mklein246@gmail.com>
2019-05-03 09:43:06 -0700

    better. content is working. on to the rest.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Card from './Card';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Card from "./Card";
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {props.cards.map(el => {
-        <Card 
-          headline={el.headline}
-          img={el.img}
-          author={el.author}
-        />
+        return (
+          <Card
+            headline={el.headline}
+            img={el.img}
+            author={el.author}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 Cards.propTypes = {
   cards: PropTypes.arrayOf(PropTypes.object).isRequired
-}
+};
 
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Tab from './Tab';
+import React from "react";
+import PropTypes from "prop-types";
+import Tab from "./Tab";
 
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-          {props.tabs.map(el => {
-            <Tab 
-              tab={el.tab}
+        {props.tabs.map(el => {
+          return (
+            <Tab
+              tab={el}
               selectedTab={props.selectedTab}
               selectTabHandler={props.selectTabHandler}
             />
-          })}
+          );
+        })}
       </div>
     </div>
   );
@@ -23,6 +25,6 @@ Tabs.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
   selectedTab: PropTypes.string.isRequired,
   selectTabHandler: PropTypes.func.isRequired
-}
+};
 
 export default Tabs;
